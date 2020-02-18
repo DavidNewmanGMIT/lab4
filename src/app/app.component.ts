ed4125de1f2185e3d27bc27f6631e97b6de155ef
David Newman <48318621+DavidNewmanGMIT@users.noreply.github.com>
2020-02-18 17:39:39 +0000

    lab 4 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'labFour';
+  count:number =0;
+  hideLabel:boolean = true;
+
+  starClick() {
+    //alert("Whats the problem?");
+    console.log("Button Pressed");
+    this.count++;
+  }
+
+  starClickTwo() {   
+    /*
+    if(this.hideLabel == true)
+    {
+      this.hideLabel = false;
+    }
+    else{
+      this.hideLabel = true;
+    }
+    */
+
+    this.hideLabel = !this.hideLabel;
+    
+  }
+
 }
+
